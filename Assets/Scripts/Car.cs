5e896f7d8e1bbbba56a0924bec01ce8f134317b9
Ethcad <brendonm@Office-iMac.local>
2017-06-30 09:38:16 -0400

    Early experiments with non-physical connection between elements of car

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+	
+	const float forwardTorquePerFixedUpdate = 1f;
+
+	[SerializeField] GameObject castor;
+
+	Vector3 castorDelta;
+	Rigidbody driveAxleRigidbody;
+
+	void Start () {
+		driveAxleRigidbody = GetComponent<Rigidbody> ();
+		castorDelta = castor.transform.position - transform.position;
+	}
+
+	void FixedUpdate () {
+		driveAxleRigidbody.AddRelativeTorque (new Vector3 (0f, forwardTorquePerFixedUpdate, 0f));
+		castor.transform.position = transform.position + castorDelta;
+	}
+}
