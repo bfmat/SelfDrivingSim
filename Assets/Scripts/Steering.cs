ac6603e02ee0870057f1764b8de7bc5c1895141a
brendon-ai <brendon-m@outlook.com>
2017-12-26 11:27:55 -0500

    Amplified deadband

--- Assets/Scripts/Steering.cs
+++ Assets/Scripts/Steering.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 static class Steering
 {
     // The width in steering angle units of the dead band
-    const float deadBandWidth = 0.05f;
+    const float deadBandWidth = 2f;
 
     // The center of the current backlash dead band in steering angle units
     static float deadBandCenter = 0f;
