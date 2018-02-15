83f11f15f50411f3c0038712f19074bcc55ae835
brendon-ai <brendon-m@outlook.com>
2018-02-15 18:07:26 -0500

    Implemented shift to edge of dead band

--- Assets/Scripts/Steering.cs
+++ Assets/Scripts/Steering.cs
@@ -5,15 +5,29 @@ static class Steering
 {
     // The width in steering angle units of the dead band
     const float deadBandWidth = 0.04f;
-
     // The center of the current backlash dead band in steering angle units
     static float deadBandCenter = 0f;
+
+    // The amount in steering angle units that the wheel should move towards the edge of the dead band per second
+    const float changePerSecond = 0.01f;
+    // The time that the sign of the input steering angle has changed
+    static float lastSteeringAngleSignChange;
+    // The last sign of the steering angle (initialize it to 0 so that it will immediately be considered a change)
+    static float lastSign = 0f;
 
     // The main function that takes a steering wheel from the steering system and returns a car wheel angle in degrees
     internal static float getWheelAngle(float steeringAngle, bool useBacklash)
     {
-        // Apply backlash to the steering angle if it is required
-        var processedSteeringAngle = useBacklash ? introduceBacklash(steeringAngle) : steeringAngle;
+        // Make a copy of the steering angle that will be modified if required
+        var processedSteeringAngle = steeringAngle;
+        // If backlash is enabled
+        if (useBacklash)
+        {
+            // Apply backlash to the steering angle
+            processedSteeringAngle = introduceBacklash(steeringAngle);
+            // Shift the steering angle to the edge of the dead band depending on how long it has been on this side of the road
+            processedSteeringAngle = moveToEdgeOfDeadBand(steeringAngle, processedSteeringAngle);
+        }
         // Multiply the processed steering angle by a constant to convert it to the approximate length of the opposite side of an angle where the wheel is in line with the hypotenuse and the adjacent is 1 unit
         var triangleOppositeLength = processedSteeringAngle * 1.038f;
         // Calculate the inverse tangent of the length of the opposite, which will equal the angle of the wheel in radians because the length of the adjacent is 1
@@ -35,5 +49,40 @@ static class Steering
         }
         // Return the center of the dead band as the steering angle, which will be in the same location as last update if it has not changed during this function call
         return deadBandCenter;
+    }
+
+    // Based on the sign of the input steering angle, move the output steering angle towards the corresponding edge of the dead band; this simulates the property of a car to head towards the ditch when it passes the crown of the road in the center
+    static float moveToEdgeOfDeadBand(float originalSteeringAngle, float steeringAngleWithBacklashApplied)
+    {
+        // Get the sign of the original steering angle
+        var sign = Mathf.Sign(originalSteeringAngle);
+        // If the sign just changed, update the relevant variables and return the steering angle with backlash without further modification
+        if (sign == lastSign)
+        {
+            lastSign = sign;
+            lastSteeringAngleSignChange = Time.timeSinceLevelLoad;
+            return steeringAngleWithBacklashApplied;
+        }
+        // Otherwise, add to the steering angle with backlash applied
+        else
+        {
+            // Multiply the delta since the last sign change by a scaling factor, and also by the negative of the new sign, which will send the car towards the side of the road that it is currently closest to
+            var delta = Time.timeSinceLevelLoad - lastSteeringAngleSignChange;
+            var offset = delta * changePerSecond * -sign;
+            // Add the delta to the steering angle, bounding at the edge of the dead band
+            var offsetSteeringAngle = steeringAngleWithBacklashApplied + offset;
+            var deadBandRadius = deadBandWidth / 2;
+            var upperLimit = deadBandCenter + deadBandRadius;
+            var lowerLimit = deadBandCenter - deadBandRadius;
+            if (offsetSteeringAngle > upperLimit)
+            {
+                offsetSteeringAngle = upperLimit;
+            }
+            if (offsetSteeringAngle < lowerLimit)
+            {
+                offsetSteeringAngle = lowerLimit;
+            }
+            return offsetSteeringAngle;
+        }
     }
 }
